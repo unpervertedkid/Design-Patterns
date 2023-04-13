8b8b43d49d2d42ffd5cbc00340834211f679f185
Brian Silah <unpervertedkid@gmail.com>
2023-04-13 14:53:47 +0300

    Using the builder pattern to simplify object creation.

--- Creational_Patterns/src/main/java/builder/Architect.java
+++ Creational_Patterns/src/main/java/builder/Architect.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class Architect {
     public static void main(String[] args) {
-        Bedroom room = new Bedroom(new Dimension(200, 100), 132, 2, Color.YELLOW, 2, 1, true, true);
+        Bedroom room = new BedroomBuilder().setDimensions(new Dimension(200, 100)).setCeilingHeight(132).setFloorNumber(2).setWallColor(Color.YELLOW).setNumberOfWindows(2).setNumberOfDoors(1).setIsDouble(true).setHasEnsuite(true).createBedroom();
 
     }
 }
---
+++ Creational_Patterns/src/main/java/builder/BedroomBuilder.java
@@ -0,0 +1,58 @@
+package builder;
+
+import java.awt.*;
+
+public class BedroomBuilder {
+    private Dimension dimensions;
+    private int ceilingHeight;
+    private int floorNumber;
+    private Color wallColor;
+    private int numberOfWindows;
+    private int numberOfDoors;
+    private boolean isDouble;
+    private boolean hasEnsuite;
+
+    public BedroomBuilder setDimensions(Dimension dimensions) {
+        this.dimensions = dimensions;
+        return this;
+    }
+
+    public BedroomBuilder setCeilingHeight(int ceilingHeight) {
+        this.ceilingHeight = ceilingHeight;
+        return this;
+    }
+
+    public BedroomBuilder setFloorNumber(int floorNumber) {
+        this.floorNumber = floorNumber;
+        return this;
+    }
+
+    public BedroomBuilder setWallColor(Color wallColor) {
+        this.wallColor = wallColor;
+        return this;
+    }
+
+    public BedroomBuilder setNumberOfWindows(int numberOfWindows) {
+        this.numberOfWindows = numberOfWindows;
+        return this;
+    }
+
+    public BedroomBuilder setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+        return this;
+    }
+
+    public BedroomBuilder setIsDouble(boolean isDouble) {
+        this.isDouble = isDouble;
+        return this;
+    }
+
+    public BedroomBuilder setHasEnsuite(boolean hasEnsuite) {
+        this.hasEnsuite = hasEnsuite;
+        return this;
+    }
+
+    public Bedroom createBedroom() {
+        return new Bedroom(dimensions, ceilingHeight, floorNumber, wallColor, numberOfWindows, numberOfDoors, isDouble, hasEnsuite);
+    }
+}
