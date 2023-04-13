4bf34ace90cd58407d8a65e4a61e55d1b40083cb
Brian Silah <unpervertedkid@gmail.com>
2023-04-13 14:07:06 +0300

    Initial Builder pattern problem

---
+++ Creational_Patterns/src/main/java/builder/Architect.java
@@ -0,0 +1,10 @@
+package builder;
+
+import java.awt.*;
+
+public class Architect {
+    public static void main(String[] args) {
+        Bedroom room = new Bedroom(new Dimension(200, 100), 132, 2, Color.YELLOW, 2, 1, true, true);
+
+    }
+}
